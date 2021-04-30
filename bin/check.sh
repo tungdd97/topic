5de6ba18b8591880f9acbcb80ee488c0485c6ae2
tungdd <tungdd@mobio.vn>
2021-04-30 23:05:22 +0700

    edit file check.sh

--- bin/check.sh
+++ bin/check.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-x``
+images_name=$1
+container_id=$(docker ps -a | grep $images_name | awk '{print $1}')
+if [[ '' == $container_id ]]; then
+  exit 1;
+fi
+# stop one running containers
+echo '####################################################'
+echo 'Stopping running containers (if available)...'
+echo '####################################################'
+docker stop $container_id
+
+# remove one stopped containers
+echo '####################################################'
+echo 'Removing containers ..'
+echo '####################################################'
+docker rm $container_id
+
+
+# remove one images
+echo '####################################################'
+echo 'Removing images ...'
+echo '####################################################'
+docker rmi $images_name
